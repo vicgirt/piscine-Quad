8dbb9174aa8953061680659f8d41b715b0872ebf
vicgirt <victor.cavalcanti@gritlab.ax>
2024-05-10 20:20:03 +0300

    Add files via upload

---
+++ QuadA.go
@@ -0,0 +1,28 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func QuadA(x, y int) {
+	if x < 0 || y < 0 {
+		return
+	}
+	for i := 0; i < y; i++ {
+		for j := 0; j < x; j++ {
+			// Will determine the carachter to print based on the position
+			var char rune
+			if i == 0 || i == y-1 {
+				char = '-'
+			} else if j == 0 || j == x-1 {
+				char = '|'
+			} else {
+				char = ' '
+			}
+			// Prints the character
+			z01.PrintRune(char)
+
+		}
+		// Prints a new line after the end of each row
+		z01.PrintRune('\n')
+	}
+
+}
